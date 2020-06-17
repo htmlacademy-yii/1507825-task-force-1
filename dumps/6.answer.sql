1b4c136c4daee4b25a220e9f2e9fca3a456e426b
kdottkachuk <omonait15@gmail.com>
2020-06-17 12:53:19 +0200

    Новые дампы

--- dumps/6.answer.sql
+++ dumps/6.answer.sql
@@ -1,70 +1,70 @@
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-05-09', '%Y-%m-%d'), 50, 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-05-09', '%Y-%m-%d'), 291, 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
 
 Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
 
-Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 16, 32 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-10-27', '%Y-%m-%d'), 491, 'Fusce consequat. Nulla nisl. Nunc nisl.
+Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 9, 6 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-10-27', '%Y-%m-%d'), 920, 'Fusce consequat. Nulla nisl. Nunc nisl.
 
 Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
 
-In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 11, 32 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-11-02', '%Y-%m-%d'), 652, 'Fusce consequat. Nulla nisl. Nunc nisl.
+In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 10, 13 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-11-02', '%Y-%m-%d'), 356, 'Fusce consequat. Nulla nisl. Nunc nisl.
 
 Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
 
-In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 16, 44 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-06-04', '%Y-%m-%d'), 670, 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
+In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 3, 4 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-06-04', '%Y-%m-%d'), 317, 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
 
 Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.
 
-Fusce consequat. Nulla nisl. Nunc nisl.', 14, 43 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-10-09', '%Y-%m-%d'), 295, 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 18, 35 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-07-16', '%Y-%m-%d'), 660, 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+Fusce consequat. Nulla nisl. Nunc nisl.', 3, 15 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-10-09', '%Y-%m-%d'), 587, 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 6, 19 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-07-16', '%Y-%m-%d'), 468, 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
 
 In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.
 
-Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.', 18, 39 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-01-22', '%Y-%m-%d'), 920, 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 17, 35 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-06-11', '%Y-%m-%d'), 598, 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
+Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.', 10, 10 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-01-22', '%Y-%m-%d'), 209, 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 10, 14 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-06-11', '%Y-%m-%d'), 394, 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
 
 Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
 
-Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.', 15, 34 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-02-16', '%Y-%m-%d'), 449, 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
+Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.', 3, 9 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-02-16', '%Y-%m-%d'), 321, 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
 
 Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
 
-Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 11, 33 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-07-16', '%Y-%m-%d'), 687, 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 2, 12 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-07-16', '%Y-%m-%d'), 736, 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
 
-Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 19, 45 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-11-11', '%Y-%m-%d'), 107, 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', 11, 33 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-11-01', '%Y-%m-%d'), 126, 'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.
+Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 1, 13 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-11-11', '%Y-%m-%d'), 355, 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.', 1, 20 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-11-01', '%Y-%m-%d'), 623, 'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.
 
 Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.
 
-Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.', 15, 33 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-10-05', '%Y-%m-%d'), 237, 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.', 3, 19 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-10-05', '%Y-%m-%d'), 210, 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
 
-Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.', 11, 37 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-02-28', '%Y-%m-%d'), 694, 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
+Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.', 2, 17 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-02-28', '%Y-%m-%d'), 100, 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
 
-Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 13, 39 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-07-04', '%Y-%m-%d'), 49, 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 9, 6 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-07-04', '%Y-%m-%d'), 920, 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.
 
 Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.
 
-Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.', 17, 35 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-07-30', '%Y-%m-%d'), 632, 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.
+Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.', 6, 6 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-07-30', '%Y-%m-%d'), 991, 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.
 
-Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', 13, 37 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-07-10', '%Y-%m-%d'), 275, 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
+Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', 7, 12 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-07-10', '%Y-%m-%d'), 830, 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
 
-Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', 18, 38 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-09-15', '%Y-%m-%d'), 476, 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.', 18, 26 );
+Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', 7, 19 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-09-15', '%Y-%m-%d'), 635, 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.', 4, 18 );
 INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2018-10-16', '%Y-%m-%d'), 371, 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
 
-In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.', 20, 36 );
-INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-02-13', '%Y-%m-%d'), 683, 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.', 2, 15 );
+INSERT INTO answer( `created_at`, `budget`, `description`, `task`, `user` ) VALUES ( STR_TO_DATE('2019-02-13', '%Y-%m-%d'), 232, 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
 
-Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 17, 28 );
+Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 3, 15 );
